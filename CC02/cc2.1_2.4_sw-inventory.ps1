eb054a2787abc17b9726a9e288640802e16af389
Rob VandenBrink <rob@coherentsecurity.com>
2019-10-02 14:08:36 -0400

    Update cc2.1_2.4_sw-inventory.ps1

--- CC02/cc2.1_2.4_sw-inventory.ps1
+++ CC02/cc2.1_2.4_sw-inventory.ps1
@@ -5,7 +5,7 @@ foreach ($pc in $pcs) {
     $i+=1
     write-host $i
     if (Test-Connection -ComputerName $pc.DNSHostName -count 2 -Quiet) {
-    $appsonpc = Get-WmiObject -Class Win32_Product -computername . | select pscomputername, vendor, name, version, installdate
+    $appsonpc = Get-WmiObject -Class Win32_Product -computername $pc.DNSHostName | select pscomputername, vendor, name, version, installdate
     $domainapps += $appsonpc
 }
 }
