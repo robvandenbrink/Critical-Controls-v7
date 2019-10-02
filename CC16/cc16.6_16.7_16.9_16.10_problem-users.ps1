253a40a5fd044c7991ffe9c65401476e519a6ea0
Rob VandenBrink <rob@coherentsecurity.com>
2019-10-02 12:55:13 -0400

    Delete cc16.6_16.7_16.9_16.10_problem-users.ps1

--- CC16/cc16.6_16.7_16.9_16.10_problem-users.ps1
+++
@@ -1,2 +0,0 @@
-get-aduser -filter * -properties * | select samaccountname, name, enabled, scriptpath, passwordlastset, passwordexpired, passwordneverexpires, passwordnotrequired, lockedout, lastlogon, lastlogondate, lastlogontimestamp, lockedout, cannotchangepassword, accountexpirationdate, mobilephone, officephone, telephoneNumber | export-csv accountinfo.csv
-
