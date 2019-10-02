26a69f5bf63143bd0433345dadfc67c01172796a
Rob VandenBrink <rob@coherentsecurity.com>
2019-10-02 13:50:02 -0400

    Update cc9.1_9.4-fw-rules.ps1

--- CC09/cc9.1_9.4-fw-rules.ps1
+++ CC09/cc9.1_9.4-fw-rules.ps1
@@ -1,5 +1,6 @@
 $pcs = get-adcomputer -filter * -property Name,OperatingSystem,Operatingsystemversion,IPV4Address
 $fwinfo = @()
+$fwrules = @()
 $i=0
 foreach ($pc in $pcs) {
     $i+=1
@@ -7,11 +8,22 @@ foreach ($pc in $pcs) {
     $tempval = new-object psobject
     if (Test-Connection -ComputerName $pc.DNSHostName -count 2 -Quiet) {
     $Computer = $pc.DNSHostname
-    $tempval = Get-NetFirewallProfile -ComputerName $Computer -PolicyStore activestore | select name, enabled, defaultinboundaction, DefaultOutboundAction
-    $tempval | add-member -membertype noteproperty -name HostName -value $pc.dnshostname
-    $tempval | add-member -membertype noteproperty -name OperatingSystem -value $pc.OperatingSystem
-    $tempval | add-member -membertype noteproperty -name IpAddress -value $pc.IPV4Address
+    # get state
+    $tempval = Get-NetFirewallProfile -cimsession $Computer -PolicyStore activestore | select name, enabled, defaultinboundaction, DefaultOutboundAction
+    # get rules
+    $tempval2 = get-NetFirewallRule -cimsession $Computer 
+    # add workstation fields
+    $tempval2 | add-member -membertype noteproperty -name HostName -value $Computer
+    $tempval2 | add-member -membertype noteproperty -name OperatingSystem -value $pc.OperatingSystem
+    $tempval2 | add-member -membertype noteproperty -name IpAddress -value $pc.IPV4Address
+    if ($tempval.count -gt 0) {
+        $tempval | add-member -membertype noteproperty -name HostName -value $pc.dnshostname
+        $tempval | add-member -membertype noteproperty -name OperatingSystem -value $pc.OperatingSystem
+        $tempval | add-member -membertype noteproperty -name IpAddress -value $pc.IPV4Address
+        }
+           
     $fwinfo += $tempval
+    $fwrules += $tempval2
 }
 }
 $fwinfo | export-csv -path ./fwstate.csv
