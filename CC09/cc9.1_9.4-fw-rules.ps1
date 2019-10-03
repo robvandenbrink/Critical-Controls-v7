a87ac5f39a1d0fa09734e54f28ffbba81bbd2e41
Rob VandenBrink <rob@coherentsecurity.com>
2019-10-03 10:53:29 -0400

    Update cc9.1_9.4-fw-rules.ps1

--- CC09/cc9.1_9.4-fw-rules.ps1
+++ CC09/cc9.1_9.4-fw-rules.ps1
@@ -1,12 +1,19 @@
 $pcs = get-adcomputer -filter * -property Name,OperatingSystem,Operatingsystemversion,IPV4Address
 $fwinfo = @()
 $fwrules = @()
+# might need to supply domain credentials to authentication the CIM Session
+# either input them manually here, or supply them in a stored credential file
+# $creds = Get-Credential
 $i=0
 foreach ($pc in $pcs) {
     $i+=1
     write-host $i
     $tempval = new-object psobject
     if (Test-Connection -ComputerName $pc.DNSHostName -count 2 -Quiet) {
+    # depending on the situation, you may need to add "-credential $creds" to the CIMSession, 
+    # where $creds holds the required credentials.  This can be inputted manually (see above)
+    # or from a stored credential file
+    # test without first in your environment, may not be required
     $TGTComputer = New-CIMSession -Computername $pc.DNSHostname
     # get state
     $tempval = Get-NetFirewallProfile -cimsession $TGTComputer -PolicyStore activestore | select name, enabled, defaultinboundaction, DefaultOutboundAction
