da902e1d4f7d45df0b4275397c20abb83d632bf7
Rob VandenBrink <rob@coherentsecurity.com>
2022-01-20 10:56:54 -0500

    Update CC03 patch  dates
    updated progress indicators, fixed indents

--- CC03/cc3.4-patch-dates.ps1
+++ CC03/cc3.4-patch-dates.ps1
@@ -1,21 +1,25 @@
 $pcs = get-adcomputer -filter * -property Name,OperatingSystem,Operatingsystemversion,LastLogonDate,IPV4Address
 $patchinfo = @()
 $i=0
+$count = $pcs.count
 foreach ($pc in $pcs) {
     $i+=1
-    write-host $i
+    # keep total progress count 
+    write-host "Host" $i "of" $count "is being assessed"
     if (Test-Connection -ComputerName $pc.DNSHostName -count 2 -Quiet) {
-    $tempval = new-object psobject
-    $lasthf = get-hotfix -computername $pc.dnshostname | sort InstalledOn | select -last 1  
-    $tempval | add-member -membertype noteproperty -name Name -value $pc.dnshostname
-    $tempval | add-member -membertype noteproperty -name PatchDate -value $lasthf.installedon
-    $tempval | add-member -membertype noteproperty -name OperatingSystem -value $pc.OperatingSystem
-    $tempval | add-member -membertype noteproperty -name OperatingSystemVersion -value $pc.OperatingSystemVersion
-    $tempval | add-member -membertype noteproperty -name IpAddress -value $pc.IPV4Address
-    $tempval | add-member -membertype noteproperty -name LastLogonDate -value $pc.LastLogonDate
-    $patchinfo += $tempval
-}
-}
+        # echo the host being assessed (only live hosts hit this print)
+        write-host $pc.dnshostname "is being assessed"
+        $tempval = new-object psobject
+        $lasthf = get-hotfix -computername $pc.dnshostname | sort InstalledOn | select -last 1  
+        $tempval | add-member -membertype noteproperty -name Name -value $pc.dnshostname
+        $tempval | add-member -membertype noteproperty -name PatchDate -value $lasthf.installedon
+        $tempval | add-member -membertype noteproperty -name OperatingSystem -value $pc.OperatingSystem
+        $tempval | add-member -membertype noteproperty -name OperatingSystemVersion -value $pc.OperatingSystemVersion
+        $tempval | add-member -membertype noteproperty -name IpAddress -value $pc.IPV4Address
+        $tempval | add-member -membertype noteproperty -name LastLogonDate -value $pc.LastLogonDate
+        $patchinfo += $tempval
+        }
+    }
 $patchinfo | export-csv -path ./patchdate.csv
 
 
